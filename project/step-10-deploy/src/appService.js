3767529ca22f683f3bf474bd9d6c8375fbd5524e
kati frantz <bahdcoder@gmail.com>
2018-12-23 15:13:01 +0100

    improve some areas of the code base

---
+++ project/step-10-deploy/src/appService.js
@@ -0,0 +1,54 @@
+import firebase from './firebase'
+
+export default new class AppService {
+  login(email, password) {
+    return firebase.auth().signInWithEmailAndPassword(email, password)
+  }
+
+  subscribeToPosts(callback) {
+    firebase.database().ref("posts").on("value", snapshot => {
+      const posts = snapshot.val();
+      const newStatePosts = [];
+      for (let post in posts) {
+        newStatePosts.push({
+          key: post,
+          slug: posts[post].slug,
+          title: posts[post].title,
+          content: posts[post].content
+        });
+      }
+      callback(newStatePosts)
+    });
+  }
+  
+  logout() {
+    return firebase.auth.signOut()
+  }
+
+  getNewSlugFromTitle = title =>
+    encodeURIComponent(
+      title
+        .toLowerCase()
+        .split(" ")
+        .join("-")
+    );
+
+  savePost = (post)  => {
+    return firebase.database().ref("posts").push({
+      ...post,
+      slug: this.getNewSlugFromTitle(post.title)
+    })
+  }
+
+  deletePost(post) {
+    return firebase.database().ref(`posts/${post.key}`).remove()
+  }
+
+  updatePost(post) {
+    return firebase.database().ref(`posts/${post.key}`).update({
+      title: post.title,
+      slug: this.getNewSlugFromTitle(post.title),
+      content: post.content
+    })
+  }
+}()
